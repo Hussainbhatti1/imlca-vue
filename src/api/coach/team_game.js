ff683c71e2fc2a65f5411c5cd6cc4f0f3e593ab6
Hussain Bhatti <hussain.bhatti@thedevden.co>
2019-01-31 13:12:43 +0500

    Add team_game api for coach side

--- src/api/coach/team_game.js
+++ src/api/coach/team_game.js
@@ -8,3 +8,16 @@ export function editTeamGame(id, game_id) {
     method: 'get',
   })
 }
+
+export function updateTeamGame(id, game_id, team_game) {
+  debugger
+  return request({
+    url: `api/v1/coaches/games/${game_id}/team_games/${id}`,
+    headers: { "AUTH-TOKEN": getToken() },
+    method: 'put',
+    data: JSON.stringify({"team_game": { team_id: team_game.team_id,
+      game_id: team_game.game_id, 
+      game_team_athletes_attributes: team_game.game_team_athletes
+        }})
+  })
+}
--- src/views/team_games/edit.vue
+++ src/views/team_games/edit.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="app-container">
     <h1 align="center" class="sub-heading-2">{{ this.form.team_title }}</h1>
-    <div class="mx-auto" v-for="n in 10">
+    <div class="mx-auto" v-for="game_athlete in form.game_team_athletes">
       <el-row>
         <el-col :span="24">
-          <el-form ref="form" :model="form" label-width="80px">
+          <el-form ref="form" :model="form" label-width="85px">
             <el-col :sm="6">
-              <el-form-item label="Athlete">
-                <el-select v-model="form.athlete" placeholder="select athlete">
-                  <el-option label="A-1" value="athlete 1"/>
-                  <el-option label="A-2" value="athlete 2"/>
+              <el-form-item label="Athlete"> 
+                 <el-select v-model="game_athlete.athlete_team_id" class="w-100" placeholder="select athlete">
+                    <el-option
+                      v-for="athlete in form.athletes"
+                      :label="athlete[0]"
+                     :value="athlete[1]">
+                    </el-option>
                 </el-select>
               </el-form-item>
             </el-col>  
@@ -17,7 +20,7 @@
             <el-col :sm="6" class="heading-1">
               <div>
                 <el-form-item label="Turnovers">
-                  <el-input type="number" v-model="form.turnovers"></el-input>
+                  <el-input type="number" v-model="game_athlete.turnovers"></el-input>
                 </el-form-item>
               </div> 
             </el-col>
@@ -25,7 +28,7 @@
             <el-col :sm="4" class="heading-1">
               <div>
                 <el-form-item label="Goals">
-                  <el-input type="number" v-model="form.goals"></el-input>
+                  <el-input type="number" v-model="game_athlete.goals"></el-input>
                 </el-form-item>
               </div> 
             </el-col>
@@ -33,7 +36,7 @@
             <el-col :sm="4" class="heading-1">
               <div>
                 <el-form-item label="Assists">
-                  <el-input type="number" v-model="form.assists"></el-input>
+                  <el-input type="number" v-model="game_athlete.assists"></el-input>
                 </el-form-item>
               </div> 
             </el-col>
@@ -41,7 +44,7 @@
             <el-col :sm="4" class="heading-1">
               <div>
                 <el-form-item label="Saves">
-                  <el-input type="number" v-model="form.saves"></el-input>
+                  <el-input type="number" v-model="game_athlete.saves"></el-input>
                 </el-form-item>
               </div> 
             </el-col>
@@ -49,50 +52,56 @@
         </el-col>    
       </el-row>
     </div>
-    <el-col :span="24" class="text-center mb-3">
+    <el-col :span="24" class="text-center m-bottom">
       <div class="mx-auto" @click="onSubmit" style="cursor: pointer;">
-        <el-button type="primary" round>Edit</el-button>
+        <el-button size="large" type="primary" round>Update</el-button>
       </div>
     </el-col>
   </div>
 </template>
 
 <script>
-  import { editTeamGame } from '@/api/coach/team_game'
+  import { editTeamGame, updateTeamGame } from '@/api/coach/team_game'
 
   export default {
     data() {
     return {
       form: {
         game: [],
-        team_game: [],
+        game_id: '',
+        team_id: '',
         team_title: '',
-        athletes: []
+        athletes: [],
+        game_team_athletes: []
       }
     }
     },
     created() {
-      this.fetchData()
+      this.fetchData(this.$route.params.id, this.$route.params.game_id)
     },
     methods: {
       onSubmit() {
+        updateTeamGame(this.$route.params.id, this.$route.params.game_id, this.form)
         this.$message('submit!')
       },
+
       onCancel() {
         this.$message({
           message: 'cancel!',
           type: 'warning'
         })
       },
+
       fetchData() {
         this.listLoading = true
         editTeamGame(this.$route.params.id, this.$route.params.game_id).then(response => {
           debugger
           this.form.game = response.game
-          this.form.team_game = response.team_game
-          this.form.divisions = response.divisions
+          this.form.game_id = response.game_id
+          this.form.team_id = response.team_id
           this.form.team_title = response.team_title
           this.form.athletes = response.athletes
+          this.form.game_team_athletes = response.game_team_athletes
           this.listLoading = false
         })
       }
@@ -105,6 +114,10 @@
     margin-left:auto;
     margin-right:auto;
     width:50%;
+}
+
+.m-bottom {
+  margin-bottom: 10px;
 }
 
 .input-group-text {
